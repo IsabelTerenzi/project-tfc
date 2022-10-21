62c5e401ff588f8b76f3cf21401979d92132ba37
Isabel Terenzi <bel.terenzi@gmail.com>
2022-10-21 17:07:11 -0300

    configuração inicial: cria Dockerfiles, Migrations e renomeia Seeders

---
+++ app/backend/src/database/migrations/create-match.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+
+      homeTeam: {
+        allowNull: false,
+        foreignKey: true,
+        field: 'home_team',
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'teams',
+          key: 'id'
+        }
+      },
+
+      homeTeamGoals: {
+        allowNull: false,
+        field: 'home_team_goals',
+        type: Sequelize.INTEGER
+      },
+
+      awayTeam: {
+        allowNull: false,
+        field: 'away_team',
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: 'teams',
+          key: 'id'
+        }
+      },
+
+      awayTeamGoals: {
+        allowNull: false,
+        field: 'away_team_goals',
+        type: Sequelize.INTEGER
+      },
+
+      inProgress: {
+        allowNull: false,
+        field: 'home_tema_goals',
+        type: Sequelize.BOOLEAN
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Matches');
+  }
+}
